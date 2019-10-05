6545eebc3da0915712a4097d2d66571629019251
elly <suhyeon1740@gmail.com>
2019-10-05 20:36:27 +0900

    css 작업 및 year 추가

--- src/App.js
+++ src/App.js
@@ -2,21 +2,39 @@ import React, {Component} from 'react';
 import './App.css';
 import Movie from './Movie'
 
-
 class App extends Component {   
   state = {    
     
   }
   componentDidMount() {
-    fetch('https://yts.lt/api/v2/list_movies.json?sort_by=rating')
-    .then(res => res.json())
-    .then(json => console.log(json))
-    .catch(err => console.log(err))
+    this._getMovies()
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    console.log(movies)
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.lt/api/v2/list_movies.json?sort_by=rating')
+      .then(res => res.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err))
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map((movie) => {
+      return <Movie 
+        title={movie.title_english} 
+        poster={movie.medium_cover_image} 
+        key={movie.id} 
+        genres={movie.genres}
+        synopsis={movie.synopsis}
+        year={movie.year}
+      />
     })
     return movies
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,27 +1,58 @@
 import React from 'react'
 import './Movie.css'
 import PropsType from 'prop-types'
+import LinesEllipsis from 'react-lines-ellipsis'
 
 Movie.PropsType = {
     title: PropsType.string.isRequired,
-    poster: PropsType.string.isRequired
+    poster: PropsType.string.isRequired,
+    genres: PropsType.array.isRequired,
+    synopsis: PropsType.string.isRequired,
+    year: PropsType.number.isRequired
 }
-function Movie({title, poster}) {
+function Movie({ title, poster, genres, synopsis, year}) {
     return (
-        <div>                
-            <h1>{title}</h1>
-            <MoviePoster poster={poster}/>
+        <div className='movie'>                           
+            <MoviePoster poster={poster} alt={title} />
+            <div className='data'>
+                <h1>{title}</h1>
+                <div>{year}</div>
+                <div className='genres'>
+                    {genres.map((genre, index) => <MovieGener genre={genre} key={index} />)}
+                </div>
+                <div>
+                    <LinesEllipsis
+                        text={synopsis}
+                        maxLine='3'
+                        ellipsis='...'
+                        trimRight
+                        basedOn='letters'
+                    />
+                </div>
+            </div>
+            
+            
         </div>
     )
 }
 
 MoviePoster.PropsType = {    
-    poster: PropsType.string.isRequired
+    poster: PropsType.string.isRequired,
+    alt: PropsType.string
 }
-function MoviePoster({poster}) {
+function MoviePoster({poster, alt}) {
     return (
-            <img src={poster} alt="movie poster" />
+        <img src={poster} alt={alt} title={alt} className='poster'/>
         )
+}
+
+MovieGener.PropsType = {
+    gener: PropsType.string.isRequired
+}
+function MovieGener({genre}) {
+    return (
+        <span>{genre }</span>
+    )
 }
 
 export default Movie
