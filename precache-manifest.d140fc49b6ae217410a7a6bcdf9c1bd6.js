d013c6139c00c3b7f9f1f2d19051a1b289325557
elly <suhyeon1740@gmail.com>
2019-10-05 20:49:21 +0900

    Updates

---
+++ precache-manifest.d140fc49b6ae217410a7a6bcdf9c1bd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50d9be575a74f1bbbf7ec01ad8f4f03d",
+    "url": "/Movie-App/index.html"
+  },
+  {
+    "revision": "4322934db3b31db39b90",
+    "url": "/Movie-App/static/css/main.7e55ea3f.chunk.css"
+  },
+  {
+    "revision": "e3d5be1b8b141c6a4bea",
+    "url": "/Movie-App/static/js/2.25b4d2b6.chunk.js"
+  },
+  {
+    "revision": "4322934db3b31db39b90",
+    "url": "/Movie-App/static/js/main.a3ea59cc.chunk.js"
+  },
+  {
+    "revision": "a6c0588567d8e2342fdc",
+    "url": "/Movie-App/static/js/runtime-main.ce5f098f.js"
+  }
+]);
